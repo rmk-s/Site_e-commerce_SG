a00be41eeb1cc382dd1d5535de410c7b195824e2
rmk-s <71083089+rmk-s@users.noreply.github.com>
2021-06-06 20:22:13 +0200

    commit test

--- express_taxi/express_taxi-vuejs/src/App.vue
+++ express_taxi/express_taxi-vuejs/src/App.vue
@@ -2,21 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <Header> </Header>
   <Accueil> </Accueil>
-  //<FormInfoReservation> </FormInfoReservation>
+  <FormInfoReservation> </FormInfoReservation>
   <Footer></Footer>
 </template>
 
 <script>
 import Accueil from './components/Accueil.vue'
 import Footer from './components/Footer.vue'
-//import FormInfoReservation from './components/FormInfoReservation.vue'
+import FormInfoReservation from './components/FormInfoReservation.vue'
 import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    Footer, Header, Accueil
-    //FormInfoReservation,
+    Footer, Header, Accueil, FormInfoReservation
   }
 }
 </script>
